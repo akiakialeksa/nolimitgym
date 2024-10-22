93872c9caa82100432f5da5a7cc9472b1ce78000
Aleksa Mišković <amiskovic@smart.edu.rs>
2024-10-22 10:21:25 +0200

    Završen HTML forme

--- index.php
+++ index.php
@@ -119,9 +119,9 @@
                         <div class="dropdown">
                             <select class="input-field dropdown-menu" id="Tip" name="Tip" autocomplete="off" required>
                                 <option value="" disabled selected>Izaberite tip vežbe</option>
-                                <option class="dropdown-item vezba-select" value="Ruke">Ruke</option>
-                                <option class="dropdown-item vezba-select" value="Noge">Noge</option>
-                                <option class="dropdown-item vezba-select" value="Kardio">Kardio</option>
+                                <option class="vezba-select" value="Ruke">Ruke</option>
+                                <option class="vezba-select" value="Noge">Noge</option>
+                                <option class="vezba-select" value="Kardio">Kardio</option>
                             </select>
                         </div>
                     </div>
@@ -132,7 +132,39 @@
                     </div>
                     <div class="form-group">
                         <label for="Vreme" class="form-label">Vreme</label>
-                        <input class="input-field" type="number" name="Vreme" id="Vreme" placeholder="12" autocomplete="off" required>
+                        <select class="input-field" id="Vreme" name="Vreme" required>
+                            <option value="" disabled selected>Izaberi vreme</option>
+                            <option value="07:00">07:00</option>
+                            <option value="07:30">07:30</option>
+                            <option value="08:00">08:00</option>
+                            <option value="08:30">08:30</option>
+                            <option value="09:00">09:00</option>
+                            <option value="09:30">09:30</option>
+                            <option value="10:00">10:00</option>
+                            <option value="10:30">10:30</option>
+                            <option value="11:00">11:00</option>
+                            <option value="11:30">11:30</option>
+                            <option value="12:00">12:00</option>
+                            <option value="12:30">12:30</option>
+                            <option value="13:00">13:00</option>
+                            <option value="13:30">13:30</option>
+                            <option value="14:00">14:00</option>
+                            <option value="14:30">14:30</option>
+                            <option value="15:00">15:00</option>
+                            <option value="15:30">15:30</option>
+                            <option value="16:00">16:00</option>
+                            <option value="16:30">16:30</option>
+                            <option value="17:00">17:00</option>
+                            <option value="17:30">17:30</option>
+                            <option value="18:00">18:00</option>
+                            <option value="18:30">18:30</option>
+                            <option value="19:00">19:00</option>
+                            <option value="19:30">19:30</option>
+                            <option value="20:00">20:00</option>
+                            <option value="20:30">20:30</option>
+                            <option value="21:00">21:00</option>
+                            <option value="21:30">21:30</option>
+                            <option value="22:00">22:00</option>
                     </div>
                     <button class="Submit" type="submit">Zakaži</button>
                 </form>
